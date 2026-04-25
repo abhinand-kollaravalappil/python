9029a6d22466992a9dd049f8ba57842af9dc0639
abhinand-kollaravalappil <nandhukvnandhukv359@gmail.com>
2026-04-25 11:06:30 +0530

    first commit

---
+++ basic.py
@@ -0,0 +1,32 @@
+#varibles
+# varibles="varibles are continers for strong data values"
+# print(varibles)
+
+# numaric type
+# age=25
+# print(age)
+
+# string type
+
+#reeting="hello world"
+#print(greeting)
+
+#boolen type
+#is _active=true
+#print(is)
+
+ #list type
+ # fruits=["apple,grape,orange"]
+ # print(fruits)
+
+#tuple type
+#coordiners=(10,20)
+#print(coordiners)
+
+#dictionary tpe
+#user={"name":"alice","age":25}
+#print(user["name"])
+
+#set type
+#colors={"red,green,blue"}
+#print(colors)
