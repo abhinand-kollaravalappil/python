7f66e566f2db8ef8af93ec1345f7f753cc1abf4f
abhinand-kollaravalappil <nandhukvnandhukv359@gmail.com>
2026-05-07 09:41:03 +0530

    updated commit issues

---
+++ python_set.py
@@ -0,0 +1,46 @@
+#creating a set
+#using curly bracket
+#my_set={1,2,3,4}
+#print(my_set)
+
+#empty set
+#empty_set=set()
+#print(type(empty_set))
+
+#2 accesing set iteams
+#my_set={10,20,30,40}
+#print(2 in my_set)
+#print(20 in my_set)
+
+#3 adding iteams to a set
+#my_set={1,2,3,4}
+#my_set.add (5)
+#print(my_set)
+
+#adding multiple iteams
+#my_set={1,2,3,4}
+#my_set.update([6,7,8])
+#print(my_set)
+
+# removing iteams from a set
+#remove()
+#my_set={1,2,3,4}
+#my_set.remove(2)
+#print(my_set)
+
+#discard
+#my_set={1,2,3,4}
+#my_set.discard(5)
+#print(my_set)
+
+#pop
+#my_set={1,2,3,4}
+#removed_item=my_set.pop()
+#print(my_set)
+
+#clear()
+#my_set={1,2,3,4}
+#my_set.clear()
+#print(my_set)
+
+
---
+++ python_tuple.py
@@ -0,0 +1,62 @@
+#creating a tuple
+#my_tuple=(1,2,3,"python",4,5)
+#print(my_tuple)
+
+#single iteam tuple
+#single_tuple=(5,)
+#print(type(single_tuple))
+
+#2 accesing tuple iteams
+#my_tuple=("apple","banana","cherry")
+#print(my_tuple[1])
+#print(my_tuple[-1])
+
+#3 uptading a tuple
+#reassinging a tuple
+#my_tuple(1,2,3)
+#my_tuple(4,5,6)
+#print(my_tuple)
+
+#convert tuple to a list
+#my_tuple=("apple","banana","cherry")
+#temp_list=list(my_tuple)
+#temp_list[1]='orange'
+#my_tuple=tuple(temp_list)
+#print(my_tuple)
+
+#4 unpacking tuples
+#my_tuple=("apple",'banana','cherry')
+#(fruit1,fruit2,fruit3)=my_tuple
+#print(fruit1)
+#print(fruit2)
+#print(fruit3)
+
+#5 using* (asterisk)
+#my_tuple=(1,2,3,4,5)
+#(first,*middle,last)=my_tuple
+#print(first)
+#print(middle)
+#print(last)
+
+#6 joinig tuples
+
+#tuple1=(1,2,3)
+#tuple2=(4,5,6)
+#joined_tuple=tuple1+tuple2
+#print(joined_tuple)
+
+#tuple methods
+#my_tuple=(1,2,3,2,2,4)
+#print(my_tuple)
+
+
+#index()
+
+#my_tuple=('apple','banana','cherry')
+#print(my_tuple.index('banana'))
+
+
+#8 deleting a tuple
+#my_tuple=('apple','banana','cherry')
+#del my_tuple
+
