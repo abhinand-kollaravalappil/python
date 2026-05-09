f12fe59260aa00600fd4831ddaa21ab573e42463
abhinand-kollaravalappil <nandhukvnandhukv359@gmail.com>
2026-05-09 11:26:08 +0530

    added condions

---
+++ conditions.py
@@ -0,0 +1,31 @@
+#conditional and control statments in python
+#conditional statments(if,elif,else)
+
+#if statment
+#x=10
+#if x>5:
+    #print("x is greater than5")
+
+    #if-else statment
+#x=3
+#if x>1:
+    #print("x is greater than 1")
+
+
+#if-elif-else statment
+
+#x=7
+#if x>10:
+  #print("x is greater than 10")
+
+#elif x>5:
+ #print("x is greater than5 but less than or equal to 10")
+
+
+
+#a=int(input("entwr a number"))
+#if a>10:
+    #print("a is greater than 10")
+
+
+
