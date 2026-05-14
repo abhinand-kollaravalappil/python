2fe659114c8bc989b10ce0118d767e26b2aca0ba
abhinand-kollaravalappil <nandhukvnandhukv359@gmail.com>
2026-05-14 10:55:48 +0530

    added loops

---
+++ conditional and control _statment.py
@@ -0,0 +1,127 @@
+#if
+
+#x=5
+#y=8
+#if x>y:
+    #print("x is greater than y")
+
+#else:
+    #print("x is lower than y")
+    
+#a=200
+#b=2001
+#if a>b:
+    #print("a is bigger value")
+#else:
+    #print("b is bigger value")
+
+#age=16
+#if age>16:
+    #print("eligible to vote")
+#else:
+    #print("not eligible to vote")
+
+    #nested statments
+#x=10
+#y=5
+#if x>5:
+    #if y<10:
+        #print("x is greter than 5 and y is less than 10")
+               
+#if with and,or,not(logical opration)
+#x=10
+#y=5
+#if x>5 and y<10:
+    #print("both conditions are true") 
+
+#if x>5 or y<10:
+    #print("at lest one condition is true")
+
+#if not x<5:
+    #print("x is not less than five") 
+
+#ternary oprater 
+#x=5
+#result="greter than 10"if x>10 else"10 or less"
+#print(result)
+
+
+#pass statment
+#x=5
+#if x>3:
+   #pass
+
+# the cotinue statmet
+#for i in range(5):
+   #if i==3:
+       #continue
+   #print(i)
+
+   #the break statment
+#for i in range(5):
+    #if i ==3:
+        #break
+    #print(i)
+
+   #loops in python
+    #for loop
+#fruits=["apple","banana","cherry"]
+#for fruit in fruits:
+   #print(fruit)
+
+#while loop
+#x=0
+#while x<5:
+    #print(x)
+    #x+=1
+#else with loops
+#for i in range (5):
+    #print(i)
+#else:
+        #print("loop is complited")
+
+#nested loops
+#for i in range(3):
+    #for j in range(2):
+        #print(f"i={i},j={j}")
+
+ #infinate loop
+#while True:
+    #print("this is a infinit loops") 
+
+
+#for loop
+
+#fruits=("apple","banana","cherry","grape")
+#for i in fruits:
+    #print(i)
+
+#numbers=(1,2,3,4,5,6,7,8,9,)
+#for i in numbers:
+    #print(numbers)
+
+
+#while loop
+
+#i=1
+#while i<=300:
+    #print(i)
+    #i+=1
+
+#n=9
+#while n<=30:
+    #print(n)
+    #n+=1
+
+#break loop
+#for i in range(11):
+    #if i==11:
+        #break
+    #print(i)
+
+#continue
+#for i in range(10):
+    #if i==7:
+        #continue
+    #print(i)
+
