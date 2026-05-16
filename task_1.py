44faaa0bdac3603692e9ad51e1adad4ac76143b6
abhinand-kollaravalappil <nandhukvnandhukv359@gmail.com>
2026-05-16 21:23:47 +0530

    added assingment_1

---
+++ task_1.py
@@ -0,0 +1,33 @@
+#number_1=int(input("enter number"))
+#number_2=int(input("enter number"))
+#oprator=input("+,-,*,/,**")
+
+#if oprator=='+':
+   #print(number_1+number_2)
+
+#elif oprator=="-":
+    #print(number_1-number_2)
+
+#elif oprator=="*":
+    #print(number_1*number_2)
+
+#elif oprator=="%":
+    #print(number_1%number_2)
+
+#elif oprator=="/":
+   #print(number_1/number_2)
+
+#elif oprator=="**":
+   #print(number_1**number_2)
+
+
+#a=[1,2,3,4,5,6,7,8]
+#b=[i for i in a if i%2==0]
+#print(b)
+
+
+#creat a list of squars from 1 to 5
+#squars=[i**2 for i in range(1,6)]
+#print(squars)
+
+#
