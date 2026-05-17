55107b053e6a115ef44268cb469c1b33d5c5e131
abhinand-kollaravalappil <nandhukvnandhukv359@gmail.com>
2026-05-17 09:48:05 +0530

    added dict_task

---
+++ dictionaries_task.py
@@ -0,0 +1,129 @@
+#student={
+    #"name":"nick",
+    #"age":20
+
+#}
+#print(student)
+
+#student={
+    #"name":"nick",
+    #"age":20,
+    #"city":"delhi"
+
+#}
+#print(student)
+ 
+#student={
+    #"name":"nick",
+    #"age":20,
+    #"city":"delhi"
+
+#}
+#print(student.get("name"))
+
+#student={
+    #"name":"nick",
+    #"age":21,
+    #"city":"delhi"
+#}
+#student.update({"age":25})
+#print(student)
+
+#student={
+    #"name":"nick",
+    #"age":20,
+    #"city":"delhi",
+#}
+#student.pop("age")
+#print(student)
+
+#student={
+    #"name":"nick",
+    #"age":20,
+    #"city":"delhi"
+#}
+#print("email"in student)
+
+#student={
+    #"name":"nick",
+    #"age":20,
+    #"city":"delhi"
+#}
+#print(student.keys())
+
+#student={
+    #"name":"nick",
+    #"age":20,
+    #"city":"delhi"
+#}
+#print(student.values())
+
+
+#keys=["name","age"]
+#values=["nick",20]
+#student=dict(zip(keys,values))
+#print(student)
+
+#data={
+    #"a":1,
+    #"b":2,
+    #"c":3 
+#}
+#print(len(data))
+
+#dict1={"a":1}
+#dict2={"b":2}
+#dict1.update(dict2)
+#print(dict1)
+
+#data={
+    #"a":1,
+    #"b":2
+#}
+#data.clear()
+#print(data)
+
+#my_dict={
+    #"x":10,
+    #"y":20
+#}
+#new_dict=my_dict.copy()
+#print(new_dict)
+
+#employee={"name":"nick","age":21}
+#salary=employee.get("salary")
+#print(salary)
+
+#my_set={"a":1,"b":2,"c":3}
+#my_set.popitem()
+#print(my_set)
+
+#student={"name":"rahul",
+#"marks":{
+#"math":90,
+    #"science":85}}
+#print(student["marks"]["science"])
+
+
+#={"name":"rahul",
+#"marks":{
+#"math":90,
+    #"science":85}}
+#student["marks"]["math"]=95
+#(student)
+
+#student={"name":"rahul",
+#"marks":{
+#"math":90,
+    #"science":85}}
+#student["marks"]["english"]=88
+#print(student)
+
+#student={"name":"rahul",
+#"marks":{
+#"math":90,
+    #"science":85}}
+#del student["marks"]["science"]
+#print(student)
+
+
