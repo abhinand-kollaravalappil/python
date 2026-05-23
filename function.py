13222e668dd8460a836653fa4590df1e7dfac24f
abhinand-kollaravalappil <nandhukvnandhukv359@gmail.com>
2026-05-23 11:09:22 +0530

    added functions_

---
+++ function.py
@@ -0,0 +1,58 @@
+#def  greet (name,age,place):
+   #print(f" my name {name},age{age},fromm{place}")
+
+#greet ("alice",21,"new york")
+
+#def greet (name,age,place):
+   #print(f"my name is{name},my age is{age},im from {place}")
+#greet("jhon",22,"kochi")
+
+
+
+#def add (a,b):
+    #"""this function adds two numbers"""
+    #return a+b
+#print(add.__doc__)
+
+
+#def cheak_number(num):
+ # if num>0:
+  #    print("positive")
+  #elif num<0:
+   #   print("negative")
+#cheak_number(10)
+#cheak_number(-5)
+
+#def cheak_even_odd(num):
+   #if num %2==0:
+    #  print("even number")
+   #elif num %2!=0:
+    #  print("odd number")
+
+#cheak_even_odd(5)
+#cheak_even_odd(12)
+
+
+# def add():
+#    print(10+20)
+# add()
+
+#add=lambda a,b:a+b
+#print(add(10,20))
+
+#lamba with_reduce()
+#numbers=[1,2,3,4]
+#sum_reduce=(lambda x,y:+y,numbers)
+#print(sum_reduce)
+
+#using filter()
+#numbers=[1,2,3,4,5,6]
+#even_numbers=filter(lambda x:x%2==0,numbers)
+#print(list(even_numbers))
+
+#higher_order_function
+#def increment(n):
+   # lambda x:x+n
+#add_5=increment(5)
+#print(add_5(10))
+
