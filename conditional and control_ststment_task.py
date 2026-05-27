ac79830813a8fd54934090828605b924ec83ea59
abhinand-kollaravalappil <nandhukvnandhukv359@gmail.com>
2026-05-27 23:50:43 +0530

    added new task

---
+++ conditional and control_ststment_task.py
@@ -0,0 +1,103 @@
+#1
+#num=8
+#if num>0:
+#    print("positive")
+#elif num<0:
+#    print("negative")
+#else :
+#    print("zero")
+
+#2
+#num=6
+#if num %2==0:
+#    print("odd")
+#else:
+#    print("even")
+
+#3
+#number=50
+
+#if number>100:
+#    print("greter than 100")
+#else:
+#    print("lower than 100")
+
+#4
+#person_age=18
+#if person_age <18:
+#    print("not eligible to vote")
+#else:
+#    print("eligible to vote")
+
+#5
+#a=33
+#b=44
+#c=22
+#print("largest number is:",max(a,b,c))
+
+#6
+#a=200
+#b=56
+#c=456
+#print("largest number is:",max(a,b,c))
+
+#7
+#mark=40
+#if mark <40:
+#    print("fail")
+#else :
+#    print("pass")
+
+#8
+#marks=(90,75,60,)
+#if marks ==90:
+#    print("a grade")
+#elif marks ==75:
+#    print("b grade")
+#elif marks ==60:
+ #   print("c grade")
+#else:
+#    print("fail") 
+
+#11
+#for i in range (1,  11):
+#    if i==6:
+#        break
+#    print(i)
+
+
+#for i in range (1, 10):
+#    if i==5:
+#        continue
+#    print(i)
+
+#13
+num=10
+if num>5:
+   pass
+print("complited")
+
+#14
+for i in range (1, 21):
+    if i %2==0:
+        print(i)
+
+
+#15
+total=0
+for i in range(1,11):
+    total=total+i
+print("sum=",total)
+
+#16
+for i in range (5):
+
+    print("hello")
+
+#18
+number=[1,2,3,4,5]
+for i in  number:
+    if i >3:
+        print(i)
+
+#19
