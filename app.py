9f1e70ade6674ada5dd243189b0e1816bb55d88c
abhinand-kollaravalappil <nandhukvnandhukv359@gmail.com>
2026-06-11 15:01:07 +0530

    aded this

--- app.py
+++ app.py
@@ -6,3 +6,38 @@
 #     return "hello buddy how are you"
 # if __name__=="__main__":
 #      app.run(debug=True)
+
+#encapsulations
+
+# class  Employe:
+
+#     def __init__(self,name,salary):
+#         self.name=name
+#         self.__salary=salary
+#     def display_employe (self):
+#         print(f"name:{self.name},salary:{self.__salary}")
+#     def update_salary(self,new_salary):
+#         self.__salary=new_salary
+# emp=Employe("jhon",50000)
+# emp.display_employe()
+
+# class Employe:
+#     def __init__(self,name,salary):
+#         self.name=name
+#         self.salary=salary
+#     def display__info(self):
+#         print(f"Employe:{self.name} {self.salary}")
+# emp=Employe("jhon",23000)
+# emp.display__info()
+
+class Employe:
+    def __init__(self,name,salary):
+        self.name=name  #public attribute
+
+        self.__salary=salary  #private attribute
+    def display__info(self):
+        print(f"Employe:{self.name} {self.__salary}")
+    def update_salary(self,new_salary):
+        self.__salary=new_salary
+emp=Employe("jhon",50000)
+emp.display__info()
